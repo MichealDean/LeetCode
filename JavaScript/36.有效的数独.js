4ba0e4fab3342054be5b730b7e47d58f5d2b7a22
unknown <wangxh@tkoffice.local>
2018-12-07 15:48:07 +0800

    Dev:修改部分代码格式

--- JavaScript/36.有效的数独.js
+++ JavaScript/36.有效的数独.js
@@ -4,9 +4,9 @@
 
 /**
  * 判断一个 9x9 的数独是否有效。只需要根据以下规则，验证已经填入的数字是否有效即可。
-     数字 1-9 在每一行只能出现一次。
-     数字 1-9 在每一列只能出现一次。
-     数字 1-9 在每一个以粗实线分隔的 3x3 宫内只能出现一次。
+ 数字 1-9 在每一行只能出现一次。
+ 数字 1-9 在每一列只能出现一次。
+ 数字 1-9 在每一个以粗实线分隔的 3x3 宫内只能出现一次。
  */
 
 /**
@@ -27,15 +27,14 @@ const isValidSudoku = function (board) {
             let val = board[i][j];
             if (val === '.') continue;
 
-            let block_no = Math.floor(i / 3) * 3 + Math.floor(j / 3);
-            if (rows[i].has(val) || cols[j].has(val) || blocks[block_no].has(val)) {
+            let blockNo = Math.floor(i / 3) * 3 + Math.floor(j / 3);
+            if (rows[i].has(val) || cols[j].has(val) || blocks[blockNo].has(val)) {
                 return false;
             }
             rows[i].add(val);
             cols[j].add(val);
-            blocks[block_no].add(val);
+            blocks[blockNo].add(val);
         }
     }
     return true;
 };
-
